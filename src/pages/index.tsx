85dab83f5f2a32030d215203b2e0b24c9e0a1a07
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-07-13 19:00:58 +0100

    update: added seo for the pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
+import Head from 'next/head';
 import BitcoinPriceProvider from '@/context/bitcoin-price-context';
 import { BitcoinPriceList } from '../components/BitcoinPriceList';
 import { BitcoinPriceHeader } from '@/components/BitcoinPriceHeader';
 
 const BitcoinPriceIndexPage = () => {
   return (
-    <BitcoinPriceProvider>
-      <div className=''>
-        <BitcoinPriceHeader />
-        <BitcoinPriceList />
-      </div>
-    </BitcoinPriceProvider>
+    <>
+      <Head>
+        <title>Bitcoin Price Index Page</title>
+        <meta
+          name='description'
+          content='show the price for BTC in USD, EUR and GBP'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <BitcoinPriceProvider>
+        <div className=''>
+          <BitcoinPriceHeader />
+          <BitcoinPriceList />
+        </div>
+      </BitcoinPriceProvider>
+    </>
   );
 };
 
--- src/pages/nft-list/index.tsx
+++ src/pages/nft-list/index.tsx
@@ -1,12 +1,20 @@
+import Head from 'next/head';
 import { NFHHeader } from '@/components/NFTHeader';
 import { NFTList } from '@/components/NFTList';
 
 const NFTListPage = () => {
   return (
-    <div className=''>
-      <NFHHeader />
-      <NFTList />
-    </div>
+    <>
+      <Head>
+        <title>My NFTS</title>
+        <meta name='description' content='List of connected wallet nfts' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className=''>
+        <NFHHeader />
+        <NFTList />
+      </div>
+    </>
   );
 };
 
